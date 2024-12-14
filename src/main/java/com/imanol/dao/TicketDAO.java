b9b3cbdd5127fcb619e31ac01e71f68f2a3f6bc8
ImanolMU@KodeBidean <kodebidean@gmail.com>
2024-12-14 05:47:44 +0100

    add dao system

--- src/main/java/com/imanol/dao/TicketDAO.java
+++ src/main/java/com/imanol/dao/TicketDAO.java
@@ -2,7 +2,17 @@ package com.imanol.dao;
 
 import com.imanol.models.Ticket;
 
+import java.util.List;
+
 public interface TicketDAO extends GenericDAO <Ticket, Integer> {
 
+    // Obtener todas las entradas de un usuario
+    List<Ticket> getTicketsByUserId(int userId);
+
+    // Obtener todas las entradas de una atracción
+    List<Ticket> getTicketsByAttractionName(String attractionName);
+
+    // Calcular el gasto medio de un usuario determinado
+    Double getAverageSpendingByUserId(int userId);
 
 }
--- src/main/java/com/imanol/dao/impl/TicketDAOImpl.java
+++ src/main/java/com/imanol/dao/impl/TicketDAOImpl.java
@@ -1,5 +1,52 @@
 package com.imanol.dao.impl;
 
-public class TicketDAOImpl {
+import com.imanol.dao.TicketDAO;
+import com.imanol.exceptions.CustomException;
+import com.imanol.models.Ticket;
+import com.imanol.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
 
+import java.util.List;
+
+public class TicketDAOImpl extends GenericDAOImpl<Ticket, Integer> implements TicketDAO {
+
+    public TicketDAOImpl() {
+        super(Ticket.class); // Pasamos la clase Ticket al constructor de GenericDAOImpl
+    }
+
+    @Override
+    public List<Ticket> getTicketsByUserId(int userId) {
+        try (Session session = HibernateUtil.getSession()) {
+            Query<Ticket> query = session.createQuery("FROM Ticket WHERE user.id = :userId", Ticket.class);
+            query.setParameter("userId", userId);
+            return query.list();
+        } catch (Exception e) {
+            throw new CustomException("Error al obtener los tickets del usuario con ID: " + userId, e);
+        }
+    }
+
+    @Override
+    public List<Ticket> getTicketsByAttractionName(String attractionName) {
+        try (Session session = HibernateUtil.getSession()) {
+            Query<Ticket> query = session.createQuery("FROM Ticket WHERE attractionName = :attractionName", Ticket.class);
+            query.setParameter("attractionName", attractionName);
+            return query.list();
+        } catch (Exception e) {
+            throw new CustomException("Error al obtener tickets para la atracción: " + attractionName, e);
+        }
+    }
+
+    @Override
+    public Double getAverageSpendingByUserId(int userId) {
+        try (Session session = HibernateUtil.getSession()) {
+            Query<Double> query = session.createQuery(
+                    "SELECT AVG(price) FROM Ticket WHERE user.id = :userId", Double.class
+            );
+            query.setParameter("userId", userId);
+            return query.uniqueResult();
+        } catch (Exception e) {
+            throw new CustomException("Error al calcular el gasto medio del usuario con ID: " + userId, e);
+        }
+    }
 }
